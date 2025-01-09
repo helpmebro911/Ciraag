a37e61487881a79144cd1335e341153a58caa396
iniridwanul <theridwanul@gmail.com>
2025-01-10 01:09:14 +0600

    Ciraag Userbot is now pluggable, allowing for easier customization and extension

--- ciraag/__main__.py
+++ ciraag/__main__.py
@@ -1,3 +1,16 @@
-from ciraag.core import handlers
+from ciraag.core.module_injector import *
+import asyncio
+import glob
 
-handlers.start_ciraag
+async def main():
+    for plugin in glob.glob("ciraag/plugins/*.py"):
+        plugin_name = plugin.replace(".py", "").replace("ciraag/plugins/", "")
+        module = __import__(f"ciraag.plugins.{plugin_name}", fromlist=[plugin_name])
+        if hasattr(module, "load_plugin"):
+            await module.load_plugin(ciraag)
+            print(f"Load {plugin_name}")
+    await ciraag.start()
+    print("Ciraag Started")
+    await asyncio.sleep(float("inf"))
+
+asyncio.run(main())
