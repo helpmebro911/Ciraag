70cd499cb9f89eb4a6c35380b604e6aa02f4ba2e
iniridwanul <theridwanul@gmail.com>
2025-01-10 01:20:44 +0600

    Removed fighter plugins

--- ciraag/plugins/fighter/__init__.py
+++
@@ -1,5 +0,0 @@
-from .reply_raid import find_opponent
-from .reply_raid import remove_opponent
-from .reply_raid import chat_fighting
-from .sbomb import slangs_bomb
-from .bomb import custom_bomb
--- ciraag/plugins/fighter/bomb.py
+++
@@ -1,9 +0,0 @@
-from ciraag.core.module_injector import *
-from ciraag.core.custom_handler import handler
-from ciraag.classes.chat_fight import Opponent
-
-@Ciraag(rf"\{handler}cbomb")
-async def custom_bomb(event):
-    await event.delete()
-    genie = Opponent()
-    await genie.bomber_genie(event)
