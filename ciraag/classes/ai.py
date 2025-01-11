eb0aac9a486da93360b75eed192240dd4d5f72dc
iniridwanul <theridwanul@gmail.com>
2025-01-11 22:22:25 +0600

    Resolved all known Gemini error cases for Android

--- ciraag/classes/ai.py
+++ ciraag/classes/ai.py
@@ -1,7 +1,13 @@
 from ciraag.core.module_injector import *
-from google import generativeai
+try:
+    from google import generativeai
+except ImportError:
+    pass
 
-gemini_api = environ["gemini_api"]
+try:
+    gemini_api = environ["gemini_api"]
+except KeyError:
+    gemini_api = ""
 
 class Gemini:
     async def google_gemini(self, event):
@@ -12,11 +18,14 @@ class Gemini:
             self.get_query = event.message.raw_text.split()
             self.get_query.pop(0)
             self.get_user_query = " ".join(self.get_query)
-            if len(self.get_user_query) == 0:
-                await ciraag.send_message(self.chat, "I haven't received any query from you. Please provide your question so I can assist you. What would you like to learn using Gemini?")
-            else:
+            try:
                 generativeai.configure(api_key=gemini_api)
-                self.model = generativeai.GenerativeModel("gemini-1.5-flash")
-                self.get_response = self.model.generate_content(f"{self.get_user_query}")
-                self.response = self.get_response.text
-                await ciraag.send_message(self.chat, f"{self.response}", parse_mode="markdown")
+                if len(self.get_user_query) == 0:
+                    await ciraag.send_message(self.chat, "I haven't received any query from you. Please provide your question so I can assist you. What would you like to learn using Gemini?")
+                else:
+                    self.model = generativeai.GenerativeModel("gemini-1.5-flash")
+                    self.get_response = self.model.generate_content(f"{self.get_user_query}")
+                    self.response = self.get_response.text
+                    await ciraag.send_message(self.chat, f"{self.response}", parse_mode="markdown")
+            except NameError:
+                await ciraag.send_message(self.chat, "Gemini is a language model that requires a specific runtime environment. To leverage its features, you would need to set up a suitable environment by installing Gemini and configuring its API.")
